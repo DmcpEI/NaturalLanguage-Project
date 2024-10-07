38c1bb59554d512ab86dcaf28b81304ae4998f06
Diogo Paixão <diogomcpaixao@gmail.com>
2024-10-07 12:43:43 +0100

    Data Loader and Manipulator

--- main.py
+++ main.py
@@ -1,28 +1,61 @@
 import pandas as pd
 
-# Assuming your tab-separated file is named 'train.txt'
-file_path = 'train.txt'
+class DataLoader:
 
+    def __init__(self, filename, column_names, target):
+
+        self.filename = filename
+        self.target = target
+        self.data = None
+        self.labels = None
+        self.column_names = column_names
+
+        self._load_data(target)
+
+    def _load_data(self, target):
+
+        try:
+
+            # Load the dataset
+            self.data = pd.read_csv(self.filename, sep='\t', names=self.column_names)
+
+            # Validate if the target column exists in the dataset
+            if target not in self.data.columns:
+                raise ValueError(f"Target column '{target}' not found in the dataset.")
+
+            self.labels = self.data[target]
+
+            print("Data loaded successfully.")
+
+        except FileNotFoundError:
+            print("File not found. Please check the file path.")
+
+class DataManipulator(DataLoader):
+
+    def __init__(self, filename, column_names, target):
+
+        try:
+
+            super().__init__(filename, column_names, target)
+            print("\nData Description:")
+            self._describe_variables()
+        except FileNotFoundError:
+            print("File not found. Please check the file path.")
+
+    def _describe_variables(self):
+
+        print("\nInformation of Data:")
+        print(self.data.info())
+
+        print("\nUnique values of features:")
+        print(self.data.nunique())
+
+        print("\nStatistical distribution of each variable:")
+        print(self.data.describe())
+
+# Assuming your tab-separated file is named 'train.txt'
+filename = 'data/train.txt'
 # Define the column names based on the structure
 column_names = ['title', 'language', 'genre', 'director', 'plot']
 
-# Read the tab-separated file into a DataFrame
-data = pd.read_csv(file_path, sep='\t', names=column_names)
-
-# Save the DataFrame to a CSV file
-csv_output_path = 'movie_data.csv'
-data.to_csv(csv_output_path, index=False)
-
-# Get the unique genres/languages
-unique_genres = data['genre'].unique()
-unique_languages = data['language'].unique()
-print(f"Unique genres: {unique_genres}")
-print(f"Unique languages: {unique_languages}")
-
-# Count how many movies fall under each genre/language
-genre_counts = data['genre'].value_counts()
-language_counts = data['language'].value_counts()
-print("\nMovies per genre:")
-print(genre_counts)
-print("\nMovies per language:")
-print(language_counts)
+data = DataManipulator(filename, column_names ,'genre')
